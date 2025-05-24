c23a072cf4249190a9f24e4dc10ea570bb25f7a4
KariSause <Ktarasuk83@gmail.com>
2025-05-24 22:38:09 +0300

    change server.js

--- server/server.js
+++ server/server.js
@@ -9,25 +9,36 @@ const connectDB = require('./db/db'); // Подключение к MongoDB
 const allowedOrigins = [
   'https://dima0073231.github.io',
   'https://dima0073231.github.io/nftgo/',
-  'http://localhost:3000' // для локальной разработки
+  'http://localhost:3000'
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    // Разрешить запросы без origin (например, из Postman)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.some(allowedOrigin => 
+      origin.startsWith(allowedOrigin) || 
+      origin.includes(allowedOrigin.replace(/https?:\/\//, ''))
+    )) {
       callback(null, true);
     } else {
+      console.error(`CORS blocked for origin: ${origin}`);
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'HEAD', 'PATCH', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 };
 
-// Middleware
 const app = express();
+
+// Важно: сначала CORS, затем другие middleware
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Предварительные запросы
 app.use(express.json());
+
 
 // Подключение к БД
 connectDB();
