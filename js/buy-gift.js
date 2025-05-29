66e661913e455a9abe6f3c15753acc4fa68cb75f
OlexandrBilyk <147237948+OlexandrBilyk@users.noreply.github.com>
2025-05-29 22:24:33 +0300

    more

--- js/buy-gift.js
+++ js/buy-gift.js
@@ -358,7 +358,7 @@ async function renderInventory(userId) {
 
       itemElement.innerHTML = `
    
-      <div class="inventory-item__wrapper flex">
+      <div class="inventory-item__wrapper">
         <span class="inventory-item__price">${gift.price} <img src="web/images/inventory/ton.svg" alt="ton" /></span>
         <img src="web/images/${gift.image}" alt="${gift.name}" class="inventory-item__img">
       </div>
